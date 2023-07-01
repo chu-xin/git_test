37d9e3cdb420da014e6a6ce650588729db49cab0
dj <2119912067@qq.com>
2023-07-02 00:03:25 +0800

    dev第一次提交

--- src/main/java/com/dj/HolleGit.java
+++ src/main/java/com/dj/HolleGit.java
@@ -4,5 +4,6 @@ public class HolleGit {
     public static void main(String[] args) {
         System.out.println("hello,Git");
         System.out.println("helle,java");
+        System.out.println("hello,master");
     }
 }
